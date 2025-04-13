Fix LiRPA robustness calculation

experiments/network_training.py
@@ -181,7 +181,8 @@ def attack_model(name: str, model: RobModel, data: Tensor, method: str, scaling_
         case "lirpa":
             distances = quantitative_lirpa(model, points=data,
                                              step_num=wandb.config.MARABOU_STEPS,
-                                             max_radius=wandb.config.MARABOU_MAX_RADIUS)
+                                             max_radius=wandb.config.MARABOU_MAX_RADIUS,
+                                             classes = get_classes(preds))
             steps = torch.zeros_like(distances)
             classes = get_classes(preds)
         case _:
@@ -260,7 +261,7 @@ def testing(dataset: str, network_type: str, method: str):
     all_labels = torch.cat(all_labels, dim=0)
     all_inputs = (torch.cat(all_inputs, dim=0))
     print(all_inputs.min(), all_inputs.max())
-    classes = attack_model(f"validation_set_{name}_{method}_std{wandb.config.SAMPLING_GN_STD}_best_____check", robust_model,
+    classes = attack_model(f"validation_set_{name}_{method}_std{wandb.config.SAMPLING_GN_STD}_best", robust_model,
                            all_inputs.clone(), method, scaled_model.temperature, all_labels.clone())
     print(f"accuracy: {(all_labels.cuda() == classes.cuda()).sum()}")
 
@@ -274,6 +275,6 @@ def testing(dataset: str, network_type: str, method: str):
     all_labels = torch.cat(all_labels, dim=0)
     all_inputs = (torch.cat(all_inputs, dim=0))
     print(all_inputs.min(), all_inputs.max())
-    classes = attack_model(f"test_{name}_{method}_std{wandb.config.SAMPLING_GN_STD}_best_____check", robust_model, all_inputs,
+    classes = attack_model(f"test_{name}_{method}_std{wandb.config.SAMPLING_GN_STD}_best", robust_model, all_inputs,
                            method, scaled_model.temperature, all_labels)
     print(f"accuracy: {(all_labels.cuda() == classes.cuda()).sum()}")

pag_robustness/robustness_oracles/Quantitative_LiRPA.py
@@ -6,31 +6,31 @@ import torch
 from maraboupy import Marabou
 
 from auto_LiRPA import PerturbationLpNorm, BoundedTensor, BoundedModule
+from torch import nn
 
 # git clone https://github.com/Verified-Intelligence/auto_LiRPA
 # cd auto_LiRPA
 # pip install .
 
-from pag_robustness.nnet_saver import nnet_exporter
-#
-#
-# def parallel_worker(model, nnet_file, point, max_radius, step_num):
-#     """ Worker function to process a single point. """
-#     radius = 0
-#     increment = max_radius / 2
-#
-#     for _ in range(step_num):
-#         # Verify l_inf ball using the pre-exported .nnet file
-#         code= verify_l_inf_ball(model, nnet_file, point, radius + increment)
-#         if code == "unsat":
-#             radius += increment
-#         increment /= 2
-#         # print(increment)
-#     # print(radius)
-#     return radius
-
 
 def find_robustness_radius(model, point, max_radius=0.1, step_num=5):
+    """ Worker function to process a single point. """
+    radius = 0
+    increment = max_radius / 2
+
+    for _ in range(step_num):
+        perturbation = PerturbationLpNorm(norm=np.inf,
+                                          x_L=torch.clip(point.unsqueeze(0) - (radius+increment), min=-0.4242),
+                                          x_U=torch.clip(point.unsqueeze(0) + (radius+increment), max=2.8215))
+        bounded_input = BoundedTensor(point.unsqueeze(0), perturbation)
+        output_bounds = model.compute_bounds(x=(bounded_input,), method="backward")
+        if max(output_bounds[1][0]) <= 0:
+            radius += increment
+        increment /= 2
+    return radius
+
+
+def fdind_robustness_radius(model, point, max_radius=0.1, step_num=5):
     low, high = 0, max_radius
     best_eps = 0
 
@@ -38,12 +38,8 @@ def find_robustness_radius(model, point, max_radius=0.1, step_num=5):
         mid = (low + high) / 2
         perturbation = PerturbationLpNorm(norm=np.inf, eps=mid)
         bounded_input = BoundedTensor(point.unsqueeze(0), perturbation)
-        # print("test_11")
         output_bounds = model.compute_bounds(x=(bounded_input,), method="backward")
-        # print("test_12")
-        # Check if classification changes
-        # print(output_bounds)
-        if torch.argmax(output_bounds[0]) == torch.argmax(output_bounds[1]):
+        if torch.max(output_bounds[0]) == torch.argmax(output_bounds[1]):
             best_eps = mid  # Still robust
             low = mid
         else:
@@ -51,34 +47,28 @@ def find_robustness_radius(model, point, max_radius=0.1, step_num=5):
     return best_eps
 
 
-def quantitative_lirpa(model, step_num, max_radius, points):
+def quantitative_lirpa(model, step_num, max_radius, points, classes):
     radii = []
-    # Export the model to a single temporary .nnet file outside of the worker
-    # with tempfile.NamedTemporaryFile(suffix=".nnet") as tmpfile:
-    #     nnet_exporter(model, tmpfile.name, points)
-    #     nnet_file = tmpfile.name
-    #     # Create a process pool and pass the file and points to workers
-    print(points[1, :].shape)
-    b_model = BoundedModule(model, torch.empty((1,784)))
-
+    class_model_list = {cls: get_bounded_class_model(model, cls, torch.empty_like(points[0].unsqueeze(0))) for cls in np.unique(classes)}
     for idx, point in enumerate(points.cuda()):
         # print("test")
-        radius = find_robustness_radius(b_model, point, max_radius, step_num)
+        radius = find_robustness_radius(class_model_list[classes[idx].item()], point, max_radius, step_num)
         radii.append(radius)
-        if idx % 10 == 0:
+        if idx % 100 == 0:
             print(f"Processed {idx} points")
-    # with concurrent.futures.ProcessPoolExecutor(max_workers=15) as executor:
-    #     futures = [
-    #         executor.submit(find_robustness_radius, BoundedModule(model, torch.empty((1, 3, 32, 32))), point, max_radius, step_num)
-    #         for point in points
-    #     ]
-    #     print("test")
-        # Collect results as they complete
-        # for idx, future in enumerate(futures):
-        #     radius = future.result()
-        #     radii.append(radius)
-        #
-        #     if idx % 10 == 0:
-        #         print(f"Processed {idx} points")
     return torch.tensor(radii)
 
+
+class WrappedModel(nn.Module):
+    def __init__(self, pretrained_model, logit_index):
+        super(WrappedModel, self).__init__()
+        self.pretrained_model = pretrained_model
+        self.logit_index = logit_index
+
+    def forward(self, x):
+        logits = self.pretrained_model(x)
+        return logits - logits[:, self.logit_index].unsqueeze(1)
+
+
+def get_bounded_class_model(model, cls, point):
+    return BoundedModule(WrappedModel(model, cls), point)