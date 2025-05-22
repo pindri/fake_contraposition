782028c0d0c62a9535702174f0bca4bc9e16a828
Pietreus <peter.blohm@tuwien.ac.at>
2025-05-22 20:52:16 +0200

    Update CIFAR10 setup

--- experiments/cifar10_experiment.py
+++ experiments/cifar10_experiment.py
@@ -10,11 +10,11 @@ WANDB_ENTITY = "peter-blohm-tu-wien"
 # print(torch.cuda.is_available())
 if __name__ == "__main__":
 
-    with open("big_net.yaml", 'r') as stream:
+    with open("cifar10_new.yaml", 'r') as stream:
         sweep_configuration = yaml.safe_load(stream)
 
-    # sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_camera_ready_cifar10_train", sweep=sweep_configuration)
-    # wandb.agent(sweep_id, function=lambda: train_network("cifar10", "resnet20"))
+    sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_camera_ready_cifar10_train", sweep=sweep_configuration)
+    wandb.agent(sweep_id, function=lambda: train_network("cifar10", "resnet20"))
 
     sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_camera_ready_cifar10_sampling", sweep=sweep_configuration)
     wandb.agent(sweep_id, function=lambda: sampling("cifar10", "resnet20","pgd"))
