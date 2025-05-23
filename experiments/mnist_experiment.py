832a0ca6add4dcb2829f991a967ec0b42e06051c
Peter Blohm <e11905150@student.tuwien.ac.at>
2025-05-23 16:46:51 +0200

    Update MNIST and LiRPA settings

--- experiments/mnist_experiment.py
+++ experiments/mnist_experiment.py
@@ -3,7 +3,7 @@ import wandb
 import torch
 if not torch.cuda.is_available():
     raise Exception("Cuda is not available")
-from network_training import train_network, sampling, temperature_scale_network, testing
+from network_training import train_network, sampling, testing
 
 WANDB_ENTITY = "peter-blohm-tu-wien"
 
@@ -11,18 +11,11 @@ if __name__ == "__main__":
     with open("mnist.yaml", 'r') as stream:
         sweep_configuration = yaml.safe_load(stream)
 
-
     # sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_mnist_training_best", sweep=sweep_configuration)
     # wandb.agent(sweep_id, function=lambda: train_network("mnist", "feed_forward"))
 
-    # sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_mnist_scaling_best", sweep=sweep_configuration)
-    # wandb.agent(sweep_id, function=lambda: temperature_scale_network("mnist", "feed_forward"))
-
     # sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_mnist_sampling_best", sweep=sweep_configuration)
     # wandb.agent(sweep_id, function=lambda: sampling("mnist", "feed_forward","pgd"))
-
-    sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_mnist_sampling_best", sweep=sweep_configuration)
-    wandb.agent(sweep_id, function=lambda: sampling("mnist", "feed_forward","pgd"))
 
     sweep_id = wandb.sweep(entity=WANDB_ENTITY, project="pag_mnist_test_best", sweep=sweep_configuration)
     wandb.agent(sweep_id, function=lambda: testing("mnist", "feed_forward","pgd"))
--- pag_robustness/robustness_oracles/Quantitative_LiRPA.py
+++ pag_robustness/robustness_oracles/Quantitative_LiRPA.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import numpy as np
 import torch
 
@@ -13,12 +15,12 @@ def find_robustness_radius(model, point, max_radius=0.1, step_num=5):
     """ Worker function to process a single point. """
     radius = 0
     increment = max_radius / 2
-
     for _ in range(step_num):
         perturbation = PerturbationLpNorm(norm=np.inf,
-                                          x_L=torch.clip(point.unsqueeze(0) - (radius+increment), min=-0.4242),
-                                          x_U=torch.clip(point.unsqueeze(0) + (radius+increment), max=2.8215))
+                                          x_L=torch.clip(point.unsqueeze(0) - (radius+increment), min=0),
+                                          x_U=torch.clip(point.unsqueeze(0) + (radius+increment), max=1))
         bounded_input = BoundedTensor(point.unsqueeze(0), perturbation)
+
         output_bounds = model.compute_bounds(x=(bounded_input,), method="backward")
         if max(output_bounds[1][0]) <= 0:
             radius += increment
@@ -34,7 +36,7 @@ def quantitative_lirpa(model, step_num, max_radius, points, classes):
         radius = find_robustness_radius(class_model_list[classes[idx].item()], point, max_radius, step_num)
         radii.append(radius)
         if idx % 100 == 0:
-            print(f"Processed {idx} points")
+            print(f"Processed {idx} points, last one has radius {radius}")
     return torch.tensor(radii)
 
 
